61d39f642fcbf4f7f05fdfb96e8143332c1c7870
SIWOO0909 <siwoo200409@gmail.com>
2024-03-30 01:32:45 +0900

    mark 0.02

--- practice_mojak_orbit/Assets/Script/MovingObject.cs
+++ practice_mojak_orbit/Assets/Script/MovingObject.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    public float destructionDelay = 60f; // 파괴까지의 딜레이
     [SerializeField] private float speed;
     public bool isLog;
 
     private void Update() // 매 프레임마다 실행
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void Start()
+    {
+        // destructionDelay 이후 DestroyObject() 메소드를 호출
+        Invoke("DestroyObject", destructionDelay);
+    }
+
+    void DestroyObject()
+    {
+        // 해당 게임 오브젝트를 파괴
+        Destroy(gameObject);
     }
 
     // 접촉시 PC가 파괴
@@ -19,6 +32,6 @@ public class MovingObject : MonoBehaviour
     //    {
     //        Destroy(collision.gameObject);
     //    }
-            
+
     //}
 }
