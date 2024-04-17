a23036641f9b46a338d00d0269dd151b09e40f02
RIKKA7061 <142869347+RIKKA7061@users.noreply.github.com>
2024-04-18 01:07:47 +0900

    mark 0.21 ( 1차 빌드 )
    다시 하기가 없지만 뭐, 아쉬운거지

---
+++ practice_mojak_orbit/Assets/Park_Hyeon_Wook/ParkHyeonWook.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkHyeonWook : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("박현욱 바보");
+    }
+}
