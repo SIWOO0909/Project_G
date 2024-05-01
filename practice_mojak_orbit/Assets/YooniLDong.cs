1a32bf53ec33da79012f812983646479c46ff071
RIKKA7061 <142869347+RIKKA7061@users.noreply.github.com>
2024-05-02 00:20:30 +0900

    윤일동 스크립트 추가함

---
+++ practice_mojak_orbit/Assets/YooniLDong.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YooniLDong : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("À±ÀÏµ¿ Â÷·Ç!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
