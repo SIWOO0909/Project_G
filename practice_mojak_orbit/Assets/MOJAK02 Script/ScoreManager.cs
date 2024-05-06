954129c014acb09106725f274605f0c05ebe2431
RIKKA7061 <142869347+RIKKA7061@users.noreply.github.com>
2024-05-06 18:42:21 +0900

    점수 완벽하게 구현 ( 버그 잡음 )
    박현욱 바보

--- practice_mojak_orbit/Assets/MOJAK02 Script/ScoreManager.cs
+++ practice_mojak_orbit/Assets/MOJAK02 Script/ScoreManager.cs
@@ -1,10 +1,18 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     Text text;
+
+    // int형 변수 점수 선언
     public int scoreValue;
+
+    // int형 변수 민수1 선언
+    public int minsu1;
+
     public Transform trackedObject;
     private float previousXPosition;
 
@@ -16,6 +24,9 @@ public class ScoreManager : MonoBehaviour
         // 점수 초기화
         scoreValue = 0;
 
+        // 민수 초기화
+        minsu1 = 0;
+
         // Text 컴포넌트 가져오기
         text = GetComponent<Text>();
     }
@@ -25,11 +36,41 @@ public class ScoreManager : MonoBehaviour
         // 현재 X 위치 가져오기
         float currentXPosition = trackedObject.position.x;
 
-        // X 위치가 증가했는지 확인
+        /* X 위치가 증가했는지 확인
         if (currentXPosition > previousXPosition)
         {
             // X 위치가 증가하면 점수 증가
             scoreValue += 1;
+        }
+
+        // X 위치가 감소했는지 확인 
+        if (currentXPosition < previousXPosition)
+        {
+            // X 위치가 감소하면 점수 감소
+            scoreValue -= 1;
+        }
+        */
+        
+        // 양수 일때만 점수 증가
+        if (scoreValue >= 0)
+        {
+            if (currentXPosition > previousXPosition)
+            {
+                // X 위치가 증가하면 민수 증가
+                minsu1 += 1;
+            }
+
+            // X 위치가 감소했는지 확인 
+            if (currentXPosition < previousXPosition)
+            {
+                // X 위치가 감소하면 민수 감소
+                minsu1 -= 1;
+            }
+
+            if (minsu1 > scoreValue)
+            {
+                scoreValue = minsu1;
+            }
         }
 
         // 이전 X 위치 업데이트
