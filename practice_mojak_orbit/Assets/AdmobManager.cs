a29828f3fbe12d40b8f2e733b2086d48249527eb
RIKKA7061 <142869347+RIKKA7061@users.noreply.github.com>
2024-05-11 05:04:17 +0900

    광고 구현

---
+++ practice_mojak_orbit/Assets/AdmobManager.cs
@@ -0,0 +1,313 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class AdmobAds : MonoBehaviour
+{
+    public TextMeshProUGUI totalCoinsTxt;
+
+    public string appID = "";
+
+
+#if UNITY_ANDROID
+    string bannerId = "ca-app-pub-3940256099942544/6300978111";
+    string interId = "ca-app-pub-3940256099942544/1033173712";
+    string rewardedId = "ca-app-pub-3940256099942544/5224354917";
+    //string nativeId = "ca-app-pub-3940256099942544/2247696110\r\n";
+
+#elif UNITY_IPHONE
+    string bannerId = "ca-app-pub-3940256099942544/2934735716";
+    string interId = "ca-app-pub-3940256099942544/4411468910";
+    string rewardedId = "ca-app-pub-3940256099942544/1712485313";
+    //string nativeId = "ca-app-pub-3940256099942544/3986624511";
+
+#endif
+
+    BannerView bannerView;
+    InterstitialAd interstitialAd;
+    RewardedAd rewardedAd;
+    NativeAd nativeAd;
+
+    private void Start()
+    {
+        //ShowCoins();
+        MobileAds.RaiseAdEventsOnUnityMainThread = true;
+        MobileAds.Initialize(initStatus =>
+        {
+
+            print("광고 준비 중..");
+        });
+
+    }
+
+    // 배너광고
+    #region Banner
+    public void LoadBannerAd()
+    {
+        // 배너광고 생성
+        CreateBannerView();
+
+        // 배너광고 이벤트
+        ListenToBannerEvents();
+
+        // 배너광고 불러오기
+        if (bannerView == null)
+        {
+            CreateBannerView();
+        }
+
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("유니티 애드몹 샘플");
+
+        print("배너광고 보이기");
+        bannerView.LoadAd(adRequest);// 배너광고를 화면으로 보여주다.
+    }
+
+
+    void CreateBannerView()
+    {
+        if (bannerView != null)
+        {
+            DestroyBannerAd();
+        }
+        bannerView = new BannerView(bannerId, AdSize.Banner, AdPosition.Bottom);
+    }
+
+    void ListenToBannerEvents()
+    {
+        bannerView.OnBannerAdLoaded += () =>
+        {
+            Debug.Log("Banner view loaded an ad with response : "
+                + bannerView.GetResponseInfo());
+        };
+        // Raised when an ad fails to load into the banner view.
+        bannerView.OnBannerAdLoadFailed += (LoadAdError error) =>
+        {
+            Debug.LogError("Banner view failed to load an ad with error : "
+                + error);
+        };
+        // Raised when the ad is estimated to have earned money.
+        bannerView.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log("Banner view paid {0} {1}." +
+                adValue.Value +
+                adValue.CurrencyCode);
+        };
+        // Raised when an impression is recorded for an ad.
+        bannerView.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Banner view recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        bannerView.OnAdClicked += () =>
+        {
+            Debug.Log("Banner view was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        bannerView.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Banner view full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        bannerView.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Banner view full screen content closed.");
+        };
+    }
+    public void DestroyBannerAd()
+    {
+        if (bannerView != null)
+        {
+            print("배너광고 삭제");
+            bannerView.Destroy();
+            bannerView = null;
+        }
+    }
+
+    #endregion 
+
+    // 전면광고
+    #region Interstitial
+
+    public void LoadInterstitalAd()
+    {
+        if (interstitialAd != null)
+        {
+            interstitialAd.Destroy();
+            interstitialAd = null;
+        }
+
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("유니티 애드몹 샘플");
+
+        InterstitialAd.Load(interId, adRequest, (InterstitialAd ad, LoadAdError error) =>
+        {
+            if (error != null || ad == null)
+            {
+                print("Interstitial ad failed to load" + error);
+                return;
+            }
+
+            print("전면광고 불러오는 중" + ad.GetResponseInfo());
+
+            interstitialAd = ad;
+            InterstitialEvent(interstitialAd);
+        });
+    }
+    public void ShowInterstitialAd()
+    {
+        if (interstitialAd != null && interstitialAd.CanShowAd())
+        {
+            interstitialAd.Show();
+        }
+        else
+        {
+            print("Intersitial ad not ready ! !");
+        }
+    }
+    public void InterstitialEvent(InterstitialAd ad)
+    {
+        // Raised when the ad is estimated to have earned money.
+        interstitialAd.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log("Interstitial ad paid {0} {1}." +
+                adValue.Value +
+                adValue.CurrencyCode);
+        };
+        // Raised when an impression is recorded for an ad.
+        interstitialAd.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Interstitial ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        interstitialAd.OnAdClicked += () =>
+        {
+            Debug.Log("Interstitial ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        interstitialAd.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Interstitial ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        interstitialAd.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Interstitial ad full screen content closed.");
+        };
+        // Raised when the ad failed to open full screen content.
+        interstitialAd.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Interstitial ad failed to open full screen content " +
+                           "with error : " + error);
+        };
+    }
+
+
+    #endregion
+
+    // 리워드광고
+    #region Rewarded
+    public void LoadRewardedAd()
+    {
+        if (rewardedAd != null)
+        {
+            rewardedAd.Destroy();
+            rewardedAd = null;
+        }
+        var adRequest = new AdRequest();
+        adRequest.Keywords.Add("unity-admob-sample");
+
+        RewardedAd.Load(rewardedId, adRequest, (RewardedAd ad, LoadAdError error) =>
+        {
+            if (error != null || ad == null)
+            {
+                print("Rewarded failed to load" + error);
+                return;
+            }
+
+            print("리워드 광고 불러오기");
+            rewardedAd = ad;
+            RewardedAdEvents(rewardedAd);
+        });
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            rewardedAd.Show((Reward Reward) =>
+            {
+                print("리워드 광고를 시청했으므로 플레이어에게 보상을 줍니다.");
+
+                GrantCoins(100);
+            });
+        }
+        else
+        {
+            print("리워드 광고가 준비가 되지 않았습니다.");
+        }
+    }
+    public void RewardedAdEvents(RewardedAd ad)
+    {
+        // Raised when the ad is estimated to have earned money.
+        ad.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log("Rewarded ad paid {0} {1}." +
+                adValue.Value +
+                adValue.CurrencyCode);
+        };
+        // Raised when an impression is recorded for an ad.
+        ad.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Rewarded ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        ad.OnAdClicked += () =>
+        {
+            Debug.Log("Rewarded ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        ad.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Rewarded ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        ad.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Rewarded ad full screen content closed.");
+        };
+        // Raised when the ad failed to open full screen content.
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Rewarded ad failed to open full screen content " +
+                           "with error : " + error);
+        };
+    }
+
+
+
+    #endregion
+
+    #region extra
+    void GrantCoins(int coins)
+    {
+        int crrCoins = PlayerPrefs.GetInt("totalCoins");
+        crrCoins += coins;
+        PlayerPrefs.SetInt("totalCoins", crrCoins);
+
+        ShowCoins();
+    }
+    void ShowCoins()
+    {
+        totalCoinsTxt.text = PlayerPrefs.GetInt("totalCoins").ToString();
+    }
+
+
+
+
+    #endregion
+}
